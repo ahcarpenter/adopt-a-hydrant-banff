e01e8f710c96ceafccd6bfcd25823d8ba6f960e6
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-10-06 11:38:42 -0400

    update validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# http://stackoverflow.com/questions/8212378/validate-us-zip-code-using-rails
+# http://geekswithblogs.net/MainaD/archive/2007/12/03/117321.aspx
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
     :trackable, :validatable
@@ -5,7 +7,7 @@ class User < ActiveRecord::Base
     :password, :password_confirmation, :remember_me, :sms_number, :area, :zip
   validates_formatting_of :email, :using => :email
   validates_formatting_of :sms_number, :using => :us_phone, :allow_blank => true
-  validates_formatting_of :zip, :using => :us_zip, :allow_blank => true
+  validates_format_of :zip, :with => /^[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1} *\d{1}[A-Z]{1}\d{1}$/, :allow_blank => true
   validates_presence_of :name
   has_many :reminders_to, :class_name => "Reminder", :foreign_key => "to_user_id"
   has_many :reminders_from, :class_name => "Reminder", :foreign_key => "from_user_id"
